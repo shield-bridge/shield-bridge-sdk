a3ecdada892e9c3056eeac523c0dd7e948ab0b99
Andrew Kishino <AndrewKishino@gmail.com>
2024-05-02 00:51:54 -0700

    Add project scaffold

---
+++ src/index.ts
@@ -0,0 +1,634 @@
+import { spawn, Thread, Worker } from 'threads';
+import { ContractMethodObject, TezosToolkit, Wallet } from '@taquito/taquito';
+import { defaults, tokensGetTokens } from '@tzkt/sdk-api';
+import BigNumber from 'bignumber.js';
+
+export const tzktApiMap = {
+  mainnet: 'https://api.tzkt.io',
+  ghostnet: 'https://api.ghostnet.tzkt.io',
+};
+
+export const saplingStateMapContract = {
+  mainnet: 'KT1WorWEWjfQqQ1X2BFQiCc4hE3DuDKQVH4U',
+  ghostnet: 'KT1WorWEWjfQqQ1X2BFQiCc4hE3DuDKQVH4U',
+};
+
+type OrderedTransactionList = [
+  ContractMethodObject<Wallet>[],
+  ContractMethodObject<Wallet>[],
+  {
+    txns: string[];
+    contract?: string;
+    token_id?: number;
+    amount?: number | string;
+  }[],
+  ContractMethodObject<Wallet>[],
+];
+
+interface ContractStorage {
+  tez: number;
+  token_fa_2: {
+    key: {
+      nat: string;
+      address: string;
+    };
+    value: number;
+  }[];
+  token_fa_1_2: {
+    [contract: string]: number;
+  };
+}
+
+enum OperationIndex {
+  UPDATE_OPERATORS_ADD_INDEX = 0,
+  APPROVE_INDEX = 1,
+  DEFAULT_INDEX = 2,
+  UPDATE_OPERATORS_REMOVE_INDEX = 3,
+}
+
+type ShieldBridgeSDKConfig = {
+  client: TezosToolkit;
+  tzktApi?: 'mainnet' | 'ghostnet';
+  minConfirmations?: number;
+  saplingStateMapContract?: string;
+} & (
+  | { saplingSecret: string; saplingMnemonic?: never }
+  | { saplingSecret?: never; saplingMnemonic: string }
+);
+
+export default class ShieldBridgeSDK {
+  private tezosClient: TezosToolkit;
+
+  saplingStateMapContract: string;
+
+  minConfirmations: number;
+
+  constructor(private config: ShieldBridgeSDKConfig) {
+    this.tezosClient = config.client;
+    this.minConfirmations = config.minConfirmations || 1;
+    this.saplingStateMapContract =
+      config.saplingStateMapContract || saplingStateMapContract.mainnet;
+    // This prevents multiple instances with a separate baseUrl since the SDK is a singleton
+    defaults.baseUrl = tzktApiMap[this.config.tzktApi || 'mainnet'];
+  }
+
+  getSaplingId = async (contract?: string, tokenId?: number) => {
+    const contractStorage: ContractStorage = await fetch(
+      `${defaults.baseUrl}/v1/contracts/${this.saplingStateMapContract}/storage`,
+    ).then((res) => res.json());
+
+    if (contract) {
+      if (tokenId !== undefined) {
+        return contractStorage.token_fa_2.find(
+          (token) =>
+            token.key.address === contract && token.key.nat === `${tokenId}`,
+        )?.value;
+      }
+      return contractStorage.token_fa_1_2[contract];
+    }
+    return contractStorage.tez;
+  };
+
+  // eslint-disable-next-line class-methods-use-this
+  getTokenMetadata = async (contract: string, tokenId?: number) => {
+    const [metadata] = await tokensGetTokens({
+      contract: {
+        eq: contract,
+      },
+      select: {
+        fields: ['metadata'],
+      },
+      ...(tokenId ? { tokenId: { eq: `${tokenId}` } } : {}),
+    });
+    return metadata;
+  };
+
+  getTokenDecimals = async (contract: string, tokenId?: number) => {
+    const { decimals } = (await this.getTokenMetadata(contract, tokenId)) as {
+      name?: string;
+      symbol?: string;
+      decimals: string;
+    };
+    return parseInt(decimals, 10);
+  };
+
+  submitSaplingShieldTransaction = async (
+    saplingDeposits: {
+      amount: number | string;
+      saplingTransactions: string[];
+      owner?: string;
+      contract?: string;
+      tokenId?: number;
+    }[],
+  ) => {
+    const dappContract = await this.tezosClient.wallet.at(
+      this.saplingStateMapContract,
+    );
+
+    const transactionList: OrderedTransactionList = [[], [], [], []];
+
+    const batch = this.tezosClient.wallet.batch();
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const saplingDeposit of saplingDeposits) {
+      const { owner, amount, saplingTransactions, contract, tokenId } =
+        saplingDeposit;
+
+      if (contract) {
+        // eslint-disable-next-line no-await-in-loop
+        const tokenContract = await this.tezosClient.wallet.at(contract);
+        if (tokenId !== undefined) {
+          // FA2 update_operators add_operator
+          transactionList[OperationIndex.UPDATE_OPERATORS_ADD_INDEX].push(
+            tokenContract.methodsObject.update_operators([
+              {
+                add_operator: {
+                  owner,
+                  operator: this.saplingStateMapContract,
+                  token_id: tokenId,
+                },
+              },
+            ]),
+          );
+          // Sapling State Contract default
+          transactionList[OperationIndex.DEFAULT_INDEX].push({
+            txns: saplingTransactions,
+            contract,
+            token_id: tokenId,
+          });
+          // FA2 update_operators remove_operator
+          transactionList[OperationIndex.UPDATE_OPERATORS_REMOVE_INDEX].push(
+            tokenContract.methodsObject.update_operators([
+              {
+                remove_operator: {
+                  owner,
+                  operator: this.saplingStateMapContract,
+                  token_id: tokenId,
+                },
+              },
+            ]),
+          );
+        } else {
+          // FA1.2 approve
+          transactionList[OperationIndex.APPROVE_INDEX].push(
+            tokenContract.methodsObject.approve({
+              value: amount,
+              spender: this.saplingStateMapContract,
+            }),
+          );
+          // Sapling State Contract default
+          transactionList[OperationIndex.DEFAULT_INDEX].push({
+            txns: saplingTransactions,
+            contract,
+          });
+        }
+      } else {
+        // Tez transaction
+        transactionList[OperationIndex.DEFAULT_INDEX].push({
+          txns: saplingTransactions,
+          amount,
+        });
+      }
+    }
+
+    transactionList.forEach((transactions, index) => {
+      /**
+       * These transactions are not yet formatted for the contract call. The default
+       * index transactions can be submitted in a single call with a list of transactions.
+       * This is being done to optimize the number of operations in the transaction.
+       */
+      if (index === OperationIndex.DEFAULT_INDEX) {
+        const nonTezTransactions: {
+          txns: string[];
+          contract?: string;
+          token_id?: number;
+        }[] = [];
+        (
+          transactions as {
+            txns: string[];
+            contract?: string;
+            token_id?: number;
+            amount?: number | string;
+          }[]
+        ).forEach(({ amount, ...rest }) => {
+          // amount is only present for tez deposits
+          if (!amount) {
+            nonTezTransactions.push(rest);
+            return;
+          }
+
+          batch.withContractCall(
+            dappContract.methodsObject.default([rest]),
+            // @ts-ignore string is an acceptible type for amount
+            { amount, mutez: true },
+          );
+        });
+        // If token deposits are present, batch them separately from tez deposits
+        if (nonTezTransactions.length) {
+          batch.withContractCall(
+            dappContract.methodsObject.default(nonTezTransactions),
+          );
+        }
+      } else {
+        // These transactions are already formatted to be included in the batch call
+        transactions.forEach((transaction) => {
+          batch.withContractCall(transaction as ContractMethodObject<Wallet>);
+        });
+      }
+    });
+
+    return batch.send().then((op) => op.confirmation(this.minConfirmations));
+  };
+
+  submitSaplingUnshieldTransaction = async (
+    saplingWithdrawals: {
+      saplingTransactions: string[];
+      contract?: string;
+      tokenId?: number;
+    }[],
+  ) => {
+    const dappContract = await this.tezosClient.wallet.at(
+      this.saplingStateMapContract,
+    );
+
+    return dappContract.methodsObject
+      .default(
+        saplingWithdrawals.map((saplingWithdrawal) => ({
+          txns: saplingWithdrawal.saplingTransactions,
+          contract: saplingWithdrawal.contract,
+          token_id: saplingWithdrawal.tokenId,
+        })),
+      )
+      .send()
+      .then((op) => op.confirmation(this.minConfirmations));
+  };
+
+  submitSaplingTransferTransaction = async (
+    saplingTransfers: {
+      saplingTransactions: string[];
+      contract?: string;
+      tokenId?: number;
+    }[],
+  ) => {
+    const dappContract = await this.tezosClient.wallet.at(
+      this.saplingStateMapContract,
+    );
+
+    return dappContract.methodsObject
+      .default(
+        saplingTransfers.map((saplingTransfer) => ({
+          txns: saplingTransfer.saplingTransactions,
+          contract: saplingTransfer.contract,
+          token_id: saplingTransfer.tokenId,
+        })),
+      )
+      .send()
+      .then((op) => op.confirmation(this.minConfirmations));
+  };
+
+  shield = async (
+    shieldParams: {
+      amount: number;
+      shieldedAddress?: string;
+      contract?: string;
+      tokenId?: number;
+      memo?: string;
+    }[],
+  ) => {
+    const shieldParamPromises = shieldParams.map(async (shieldParam) => {
+      const { amount, shieldedAddress, contract, tokenId, memo } = shieldParam;
+
+      const saplingWorker = await spawn(new Worker('./saplingWorker.js'));
+
+      const saplingId = await this.getSaplingId(contract, tokenId);
+      if (!saplingId) {
+        throw new Error('Sapling state not initialized for the token');
+      }
+
+      const skType = this.config.saplingSecret ? 'secretKey' : 'mnemonic';
+      await saplingWorker.loadSaplingSecret({
+        sk:
+          skType === 'secretKey'
+            ? this.config.saplingSecret
+            : this.config.saplingMnemonic,
+        skType,
+        saplingDetails: {
+          contractAddress: this.saplingStateMapContract,
+          memoSize: 8,
+          saplingId,
+        },
+        rpcUrl: this.tezosClient.rpc.getRpcUrl(),
+      });
+
+      // Default token decimals
+      let tokenDecimals = 6;
+      if (contract) {
+        tokenDecimals = await this.getTokenDecimals(contract, tokenId);
+      }
+
+      const unitAmount = new BigNumber(10)
+        .exponentiatedBy(tokenDecimals)
+        .times(amount)
+        .toString();
+
+      let to = shieldedAddress;
+      // If no shielded address is provided, default to the loaded sapling payment address
+      if (!to) {
+        const saplingPaymentAddress = await saplingWorker.getPaymentAddress();
+        to = saplingPaymentAddress.address;
+      }
+      const saplingTxn = await saplingWorker.prepareShieldedTransaction([
+        {
+          to,
+          amount: unitAmount,
+          memo,
+          mutez: true,
+        },
+      ]);
+
+      await Thread.terminate(saplingWorker);
+
+      const owner = await this.tezosClient.wallet.pkh();
+
+      return {
+        saplingTransactions: [saplingTxn],
+        owner,
+        amount: unitAmount,
+        contract,
+        tokenId,
+      };
+    });
+
+    const contractParams = (await Promise.all(shieldParamPromises)) as {
+      saplingTransactions: string[];
+      owner: string;
+      amount: number | string;
+      contract?: string;
+      tokenId?: number;
+    }[];
+
+    return this.submitSaplingShieldTransaction(contractParams);
+  };
+
+  unshield = async (
+    unshieldParams: {
+      amount: number;
+      unshieldedAddress?: string;
+      contract?: string;
+      tokenId?: number;
+    }[],
+  ) => {
+    const unshieldParamPromises = unshieldParams.map(async (unshieldParam) => {
+      const { amount, unshieldedAddress, contract, tokenId } = unshieldParam;
+
+      const saplingWorker = await spawn(new Worker('./saplingWorker.js'));
+
+      const saplingId = await this.getSaplingId(contract, tokenId);
+
+      const skType = this.config.saplingSecret ? 'secretKey' : 'mnemonic';
+      await saplingWorker.loadSaplingSecret({
+        sk:
+          skType === 'secretKey'
+            ? this.config.saplingSecret
+            : this.config.saplingMnemonic,
+        skType,
+        saplingDetails: {
+          contractAddress: this.saplingStateMapContract,
+          memoSize: 8,
+          saplingId,
+        },
+        rpcUrl: this.tezosClient.rpc.getRpcUrl(),
+      });
+
+      // Default token decimals
+      let tokenDecimals = 6;
+      if (contract) {
+        tokenDecimals = await this.getTokenDecimals(contract, tokenId);
+      }
+
+      const unitAmount = new BigNumber(10)
+        .exponentiatedBy(tokenDecimals)
+        .times(amount)
+        .toString();
+
+      let to = unshieldedAddress;
+      // If no unshielded address is provided, default to the wallet public key hash
+      if (!to) {
+        to = await this.tezosClient.wallet.pkh();
+      }
+      const saplingTxn = await saplingWorker.prepareUnshieldedTransaction({
+        to,
+        amount: unitAmount,
+        mutez: true,
+      });
+
+      await Thread.terminate(saplingWorker);
+
+      return {
+        saplingTransactions: [saplingTxn],
+        contract,
+        tokenId,
+      };
+    });
+
+    const contractParams = (await Promise.all(unshieldParamPromises)) as {
+      saplingTransactions: string[];
+      contract?: string;
+      tokenId?: number;
+    }[];
+
+    return this.submitSaplingUnshieldTransaction(contractParams);
+  };
+
+  transfer = async (
+    transferParams: {
+      contract?: string;
+      tokenId?: number;
+      transfers: {
+        amount: number;
+        to: string;
+        memo?: string;
+      }[];
+    }[],
+  ) => {
+    const transferParamPromises = transferParams.map(async (transferParam) => {
+      const { contract, tokenId, transfers } = transferParam;
+
+      const saplingWorker = await spawn(new Worker('./saplingWorker.js'));
+
+      const saplingId = await this.getSaplingId(contract, tokenId);
+
+      const skType = this.config.saplingSecret ? 'secretKey' : 'mnemonic';
+      await saplingWorker.loadSaplingSecret({
+        sk:
+          skType === 'secretKey'
+            ? this.config.saplingSecret
+            : this.config.saplingMnemonic,
+        skType,
+        saplingDetails: {
+          contractAddress: this.saplingStateMapContract,
+          memoSize: 8,
+          saplingId,
+        },
+        rpcUrl: this.tezosClient.rpc.getRpcUrl(),
+      });
+
+      // Default token decimals
+      let tokenDecimals = 6;
+      if (contract) {
+        tokenDecimals = await this.getTokenDecimals(contract, tokenId);
+      }
+
+      const saplingTransfers = transfers.map(({ amount, to, memo }) => {
+        const unitAmount = new BigNumber(10)
+          .exponentiatedBy(tokenDecimals)
+          .times(amount)
+          .toString();
+
+        return {
+          to,
+          amount: unitAmount,
+          memo,
+          mutez: true,
+        };
+      });
+
+      const saplingTxn =
+        await saplingWorker.prepareSaplingTransaction(saplingTransfers);
+
+      await Thread.terminate(saplingWorker);
+
+      return {
+        saplingTransactions: [saplingTxn],
+        contract,
+        tokenId,
+      };
+    });
+
+    const contractParams = (await Promise.all(transferParamPromises)) as {
+      saplingTransactions: string[];
+      contract?: string;
+      tokenId?: number;
+    }[];
+
+    return this.submitSaplingTransferTransaction(contractParams);
+  };
+
+  getShieldedBalance = async ({
+    saplingId,
+    contract,
+    tokenId,
+  }: {
+    saplingId?: number;
+    contract?: string;
+    tokenId?: number;
+  }): Promise<number> => {
+    const saplingWorker = await spawn(new Worker('./saplingWorker.js'));
+
+    let saplingIdQuery = saplingId;
+    if (!saplingIdQuery) {
+      saplingIdQuery = await this.getSaplingId(contract, tokenId);
+    }
+
+    const skType = this.config.saplingSecret ? 'secretKey' : 'mnemonic';
+    await saplingWorker.loadSaplingSecret({
+      sk:
+        skType === 'secretKey'
+          ? this.config.saplingSecret
+          : this.config.saplingMnemonic,
+      skType,
+      saplingDetails: {
+        contractAddress: this.saplingStateMapContract,
+        memoSize: 8,
+        saplingId: saplingIdQuery,
+      },
+      rpcUrl: this.tezosClient.rpc.getRpcUrl(),
+    });
+
+    const balance = await saplingWorker.getSaplingBalance();
+
+    await Thread.terminate(saplingWorker);
+
+    return balance;
+  };
+
+  getAllShieldedBalances = async () => {
+    const contractStorage: ContractStorage = await fetch(
+      `${tzktApiMap.ghostnet}/v1/contracts/${this.saplingStateMapContract}/storage`,
+    ).then((res) => res.json());
+
+    const saplingIds: {
+      saplingId: number;
+      contract?: string;
+      tokenId?: number;
+    }[] = [
+      {
+        saplingId: contractStorage.tez,
+      },
+    ];
+
+    contractStorage.token_fa_2.forEach(({ key, value }) => {
+      saplingIds.push({
+        saplingId: value,
+        contract: key.address,
+        tokenId: parseInt(key.nat, 10),
+      });
+    });
+
+    Object.entries(contractStorage.token_fa_1_2).forEach(
+      ([contract, saplingId]) => {
+        saplingIds.push({ saplingId, contract });
+      },
+    );
+
+    return Promise.all(
+      saplingIds.map(async (saplingToken) => {
+        const balance = await this.getShieldedBalance(saplingToken);
+        return {
+          balance,
+          contract: saplingToken.contract,
+          tokenId: saplingToken.tokenId,
+        };
+      }),
+    );
+  };
+
+  getShieldedTransactions = async (contract?: string, tokenId?: number) => {
+    const saplingWorker = await spawn(new Worker('./saplingWorker.js'));
+
+    const saplingId = await this.getSaplingId(contract, tokenId);
+
+    const skType = this.config.saplingSecret ? 'secretKey' : 'mnemonic';
+    await saplingWorker.loadSaplingSecret({
+      sk:
+        skType === 'secretKey'
+          ? this.config.saplingSecret
+          : this.config.saplingMnemonic,
+      skType,
+      saplingDetails: {
+        contractAddress: this.saplingStateMapContract,
+        memoSize: 8,
+        saplingId,
+      },
+      rpcUrl: this.tezosClient.rpc.getRpcUrl(),
+    });
+
+    const transactions = (await saplingWorker.getSaplingTransactions()) as {
+      incoming: {
+        isSpent: boolean;
+        value: number;
+        memo?: string;
+        paymentAddress: string;
+      }[];
+      outgoing: {
+        value: number;
+        memo?: string;
+        paymentAddress: string;
+      }[];
+    };
+
+    await Thread.terminate(saplingWorker);
+
+    return transactions;
+  };
+}
---
+++ src/saplingWorker.ts
@@ -0,0 +1,141 @@
+import { expose } from 'threads/worker';
+
+import { RpcReadAdapter } from '@taquito/taquito';
+import { SaplingToolkit, InMemorySpendingKey } from '@taquito/sapling';
+import { RpcClient } from '@taquito/rpc';
+import { Prefix, prefix, b58cencode } from '@taquito/utils';
+import * as sapling from '@airgap/sapling-wasm';
+import * as bip39 from 'bip39';
+import {
+  SaplingContractDetails,
+  ParametersSaplingTransaction,
+  ParametersUnshieldedTransaction,
+} from '@taquito/sapling/dist/types/types';
+
+const SECRET_KEY_METHOD = 'secretKey';
+const MNEMONIC_METHOD = 'mnemonic';
+
+let iMSK: InMemorySpendingKey | null;
+let sTk: SaplingToolkit | null;
+
+const createExtendedSpendingKey = async (mnemonic: string) => {
+  const fullSeed = await bip39.mnemonicToSeed(mnemonic);
+  const first32 = fullSeed.subarray(0, 32);
+  const second32 = fullSeed.subarray(32);
+  const seed = Buffer.from(
+    // eslint-disable-next-line no-bitwise
+    first32.map((byte, index) => byte ^ second32[index]),
+  );
+  const spendingKeyArr = new Uint8Array(
+    await sapling.getExtendedSpendingKey(seed, 'm/'),
+  );
+  return b58cencode(spendingKeyArr, prefix[Prefix.SASK]);
+};
+
+const loadSaplingSecret = async ({
+  sk,
+  saplingDetails,
+  rpcUrl,
+  skType = MNEMONIC_METHOD,
+}: {
+  sk: string;
+  saplingDetails: SaplingContractDetails;
+  rpcUrl: string;
+  skType: 'secretKey' | 'mnemonic';
+}) => {
+  try {
+    const secretKey = sk;
+    const loadAccountMethod = skType;
+
+    if (loadAccountMethod === SECRET_KEY_METHOD) {
+      iMSK = new InMemorySpendingKey(secretKey);
+    } else if (loadAccountMethod === MNEMONIC_METHOD) {
+      iMSK = await InMemorySpendingKey.fromMnemonic(secretKey);
+    } else {
+      throw new Error('Invalid account loading method provided');
+    }
+  } catch (err) {
+    iMSK = null;
+    sTk = null;
+    throw err;
+  }
+
+  try {
+    sTk = new SaplingToolkit(
+      { saplingSigner: iMSK },
+      saplingDetails,
+      new RpcReadAdapter(new RpcClient(rpcUrl)),
+    );
+  } catch (err) {
+    iMSK = null;
+    sTk = null;
+    throw err;
+  }
+};
+
+const getPaymentAddress = () =>
+  iMSK
+    ?.getSaplingViewingKeyProvider()
+    .then((inMemoryViewingKey) => inMemoryViewingKey.getAddress());
+
+const prepareShieldedTransaction = (
+  shieldTransactions: ParametersSaplingTransaction[],
+) =>
+  sTk
+    ?.prepareShieldedTransaction(shieldTransactions)
+    .catch((err) => console.error(err.message));
+
+const prepareUnshieldedTransaction = (
+  unshieldTransaction: ParametersUnshieldedTransaction,
+) =>
+  sTk
+    ?.prepareUnshieldedTransaction(unshieldTransaction)
+    .catch((err) => console.error(err.message));
+
+const prepareSaplingTransaction = (
+  saplingTransactions: ParametersSaplingTransaction[] = [],
+) =>
+  sTk
+    ?.prepareSaplingTransaction(saplingTransactions)
+    .catch((err) => console.error(err.message));
+
+const getSaplingBalance = () =>
+  sTk
+    ?.getSaplingTransactionViewer()
+    .then((txViewer) =>
+      txViewer.getBalance().then((balance) => balance.toNumber()),
+    )
+    .catch((err) => console.error(err.message));
+
+const getSaplingTransactions = () =>
+  sTk
+    ?.getSaplingTransactionViewer()
+    .then((txViewer) => txViewer.getIncomingAndOutgoingTransactions())
+    .then((transactionHistory) => ({
+      incoming: transactionHistory.incoming.map((tx) => ({
+        ...tx,
+        value: tx.value.toNumber(),
+      })),
+      outgoing: transactionHistory.outgoing.map((tx) => ({
+        ...tx,
+        value: tx.value.toNumber(),
+      })),
+    }))
+    .catch((err) => console.error(err.message));
+
+const reInitializeSapling = () => {
+  iMSK = null;
+  sTk = null;
+};
+
+expose({
+  createExtendedSpendingKey,
+  loadSaplingSecret,
+  getPaymentAddress,
+  prepareShieldedTransaction,
+  prepareUnshieldedTransaction,
+  prepareSaplingTransaction,
+  getSaplingBalance,
+  getSaplingTransactions,
+  reInitializeSapling,
+});
